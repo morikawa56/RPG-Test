930142e7a6b69166f0f4026dd03d463386d659fd
Aii如樱如月 <morikawa2021@163.com>
2024-10-04 11:35:42 +0800

    提交缺失文件

---
+++ Assets/Scripts/Manager/InventoryManager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    public List<ItemScriptObject> itemList;
+
+    public void AddItem(ItemScriptObject item)
+    {
+        itemList.Add(item);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerPick.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPick : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == Tag.INTERACTABLE)
+        {
+            PickableObject po = other.gameObject.GetComponent<PickableObject>();
+
+            if (po != null)
+            {
+                Transform javelinGenerator = other.gameObject.transform.parent;
+
+                InventoryManager.Instance.AddItem(po.itemSO);
+
+                // 删除标枪生成器
+                if (javelinGenerator != null)
+                {
+                    Destroy(javelinGenerator.gameObject);
+                }
+
+                Destroy(po.gameObject);
+
+            }
+        }
+    }
+}
